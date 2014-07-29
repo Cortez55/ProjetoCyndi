9b7b9447621033ad89615583bd040f5c1fbd4152
David Paiva <draxdeveloper@gmail.com>
2014-07-29 20:39:29 -0300

    assets do lunatico, para base

---
+++ Assets/Scripts/Player/skills/InvisibilitySkill.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InvisibilitySkill : BaseSkill {
+	
+	//Tempo decorrido apos ativacao da skill
+	private float tempoDecorrido;
+	
+	private float alphaMinimo;
+	
+	//Duracao do efeito de invisibilidade
+	[Range(0.1f, 3)]
+	public float duracaoEfeito;
+	
+	//Forca do efeito de invisibilidade
+	[Range(0, 1)]
+	public float forcaEfeito;
+	
+	public override void Update ()
+	{
+		base.Update();
+		
+		if (IsExecuting)
+		{
+			this.alphaMinimo = Mathf.Lerp(1, 0, forcaEfeito);
+			
+			tempoDecorrido += Time.deltaTime;
+			
+			if (tempoDecorrido < duracaoEfeito)
+			{
+				float lerp = tempoDecorrido / duracaoEfeito;
+				float alpha = Mathf.Lerp(1, alphaMinimo, lerp);
+				ChangeAlphaColorSprite(alpha);
+			}
+		}
+	}
+	
+	protected override void Execute()
+	{
+		tempoDecorrido = 0;
+	}
+	
+	protected override void PosExecute()
+	{
+		base.PosExecute();	
+		ChangeAlphaColorSprite(1f);
+	}
+	
+	private void ChangeAlphaColorSprite(float alphaValue)
+	{
+		Color color = this.GetComponent<SpriteRenderer>().color;
+		color.a = alphaValue;
+		this.GetComponent<SpriteRenderer>().color = color;
+	}
+}
---
+++ Assets/Scripts/objetos/Bullet/Bullet.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+	
+	public float Speed;
+	
+	private GameObject player;
+
+	public GameObject Alvo
+	{
+		set;
+		get;
+	}
+	
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+		transform.LookAt(Alvo.transform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float adjustedSpeed = Speed * Time.deltaTime;
+		transform.Translate (0, 0, adjustedSpeed);
+	}
+	
+	void OnCollisionEnter2D (Collision2D other)
+	{
+		if (other.gameObject.Equals(player))
+		{
+			//Destroy(player);
+		}
+	}
+	
+}
